9ce35a7cf1144dbad2833393aea05b77dc884d6f
Troels Lenda <troelslenda@gmail.com>
2017-08-15 19:28:53 +0200

    Modify view to show active and completed todos. Also create new todo form. Providing logic for view in the TodoController.

--- web/app/Http/Controllers/TodoController.php
+++ web/app/Http/Controllers/TodoController.php
@@ -3,19 +3,47 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Todo;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Auth;
 
 class TodoController extends Controller
 {
     public function index()
     {
+        $active = [];
+        $completed = [];
+        $todos = Auth::user()->todos()->orderBy('updated_at','desc')->get();
+        /** @var Collection $groups */
+        // Split the collection of todos into active and completed.
+        // @todo if possible with Laravel templating use a filter on output loops instead.
+        $groups = $todos->groupBy('completed');
+        if ($groups->get(0)) {
+            $active = $groups->get(0);
+        }
+        if ($groups->get(1)) {
+            $completed = $groups->get(1);
+        }
+
+        return view('home', ['active' => $active, 'completed' => $completed]);
     }
     public function create(Request $request)
     {
+        $todo = Todo::create(['name' => $request->get('name')]);
+        // Relate todos to loggedin user.
+        Auth::user()->todos()->save($todo);
+        return redirect('/home');
     }
     public function toggleComplete($id)
     {
+        $todo = Auth::user()->todos()->where('id', $id)->first();
+        $todo->setAttribute('completed', !$todo->getAttribute('completed'));
+        $todo->save();
+        return redirect('/home');
     }
     public function deleteCompleted()
     {
+        Auth::user()->todos()->where('completed', true)->delete();
+        return redirect('/home');
     }
 }

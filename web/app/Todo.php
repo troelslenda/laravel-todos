b040f28d1734b41a470722002e206bccbcad3f69
Troels Lenda <troelslenda@gmail.com>
2017-08-15 19:43:59 +0200

    use SoftDeletes

--- web/app/Todo.php
+++ web/app/Todo.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Symfony\Component\Routing\Exception\MissingMandatoryParametersException;
 
 class Todo extends Model

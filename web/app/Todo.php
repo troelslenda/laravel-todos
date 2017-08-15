35b02ff4298addc714f9468fa3217719b3a8cfca
Troels Lenda <troelslenda@gmail.com>
2017-08-15 18:49:51 +0200

    Setup Todo with name, completed state and relationship to User

--- web/app/Todo.php
+++ web/app/Todo.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Todo extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $dates = ['deleted_at'];
+
+    protected $table = 'todos';
+
+    protected $fillable = [
+      'name',
+      'completed',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 }

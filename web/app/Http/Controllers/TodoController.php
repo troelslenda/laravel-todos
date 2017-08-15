d21108b9a43e148abe339bbcc283cad1e0364e4e
Troels Lenda <troelslenda@gmail.com>
2017-08-15 18:56:14 +0200

    Initial routing and controller work.

--- web/app/Http/Controllers/TodoController.php
+++ web/app/Http/Controllers/TodoController.php
@@ -6,5 +6,16 @@ use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
-    //
+    public function index()
+    {
+    }
+    public function create(Request $request)
+    {
+    }
+    public function toggleComplete($id)
+    {
+    }
+    public function deleteCompleted()
+    {
+    }
 }
